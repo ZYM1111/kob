ac37d964e533f428ff2266feb15ac0928702882f
张逸敏 <18796895416@163.com>
2022-11-17 23:01:00 +0800

    游戏界面

---
+++ web/src/views/ranklist/RanklistIndexView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        排行榜
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+</style>
---
+++ web/src/views/user/bot/UserBotIndexView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        我的Bot
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+</style>
