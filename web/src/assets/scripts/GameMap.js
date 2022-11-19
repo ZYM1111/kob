cc839fac73a0af268cdd44432627f13ded1a4381
张逸敏 <18796895416@163.com>
2022-11-19 20:19:11 +0800

    画蛇 接收键盘输入移动

--- web/src/assets/scripts/GameMap.js
+++ web/src/assets/scripts/GameMap.js
@@ -1,17 +1,23 @@
 import { AcGameObject } from "./AcGameObject";
 import { Wall } from "./Wall";
+import { Snake } from "./Snake";
 
 export class GameMap extends AcGameObject {
-    constructor(ctx, parent) {  
+    constructor(ctx, parent) {
         super();
-        this.ctx = ctx; 
-        this.parent = parent;   
+        this.ctx = ctx;
+        this.parent = parent;
         this.L = 0; // 一个小方格的边长
         this.rows = 13;
-        this.cols = 13;
+        this.cols = 14;
         this.inner_walls_cnt = 20;
         this.walls = [];
         this.rand_cnt = 1000;
+
+        this.snakes = [
+            new Snake({ id: 0, r: this.rows - 2, c: 1, color: "red" }, this),
+            new Snake({ id: 1, r: 1, c: this.cols - 2, color: "blue" }, this),
+        ];
     }
 
     check_connectivity(g, sx, sy, tx, ty) {
@@ -50,13 +56,14 @@ export class GameMap extends AcGameObject {
             for (let j = 0; j < this.rand_cnt; j++) {
                 let r = parseInt(Math.random() * this.rows);
                 let c = parseInt(Math.random() * this.cols);
-                if (g[r][c] || g[c][r]) continue;
+                if (g[r][c] || g[this.rows - 1 - r][this.cols - 1 - c])
+                    continue;
                 if (
                     (r === this.rows - 2 && c === 1) ||
                     (r === 1 && c === this.cols - 2)
                 )
                     continue;
-                g[r][c] = g[c][r] = true;
+                g[r][c] = g[this.rows - 1 - r][this.cols - 1 - c] = true;
                 break;
             }
         }
@@ -86,6 +93,7 @@ export class GameMap extends AcGameObject {
 
     update() {
         this.update_size();
+        if (this.check_ready()) this.next_step();
         this.render();
     }
 
@@ -105,9 +113,64 @@ export class GameMap extends AcGameObject {
     }
 
     start() {
-        console.log("hello world");
         for (let i = 0; i < this.rand_cnt; i++) {
             if (this.create_walls()) break;
+        }
+        this.add_listening_events();
+    }
+
+    check_valid(cell) {
+        for (const wall of this.walls) {
+            if (wall.r === cell.r && wall.c === cell.c) {
+                return false;
+            }
+        }
+
+        for (const snake of this.snakes) {
+            let k = snake.cells.length;
+            if (!snake.check_tail_increasing()) {
+                k--;
+            }
+            for (let i = 0; i < k; i++) {
+                if (
+                    snake.cells[i].r === cell.r &&
+                    snake.cells[i].c === cell.c
+                ) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    add_listening_events() {
+        this.ctx.canvas.focus();
+        console.log("hello");
+        const [snake0, snake1] = this.snakes;
+        this.ctx.canvas.addEventListener("keydown", (e) => {
+            if (e.key === "w") snake0.set_direction(0);
+            else if (e.key === "d") snake0.set_direction(1);
+            else if (e.key === "s") snake0.set_direction(2);
+            else if (e.key === "a") snake0.set_direction(3);
+            else if (e.key === "ArrowUp") snake1.set_direction(0);
+            else if (e.key === "ArrowRight") snake1.set_direction(1);
+            else if (e.key === "ArrowDown") snake1.set_direction(2);
+            else if (e.key === "ArrowLeft") snake1.set_direction(3);
+            else console.log("other");
+        });
+    }
+
+    check_ready() {
+        for (const snake of this.snakes) {
+            if (snake.status !== "idle") return false;
+            if (snake.direction === -1) return false;
+        }
+        return true;
+    }
+
+    next_step() {
+        for (const sanke of this.snakes) {
+            sanke.next_step();
         }
     }
 }
