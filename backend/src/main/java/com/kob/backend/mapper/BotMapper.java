74fa8d5800595682f2b81b10d584cec03b81e680
Yimin Zhang <18796895416@163.com>
2024-04-02 02:34:13 +0800

    bot add; bot remove

--- backend/src/main/java/com/kob/backend/mapper/BotMapper.java
+++ backend/src/main/java/com/kob/backend/mapper/BotMapper.java
@@ -1,9 +1,9 @@
 package com.kob.backend.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.kob.backend.pojo.User;
+import com.kob.backend.pojo.Bot;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public interface UserMapper extends BaseMapper<User> {
+public interface BotMapper extends BaseMapper<Bot> {
 }
