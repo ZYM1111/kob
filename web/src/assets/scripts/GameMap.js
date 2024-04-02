518573575f60fda6d8dad98c1dba3f4e57ad3fd7
Yimin Zhang <18796895416@163.com>
2024-04-03 01:55:07 +0800

    后端WebSocketServer的jwt验证 前端匹配页面交互 server生成地图发送给client

--- web/src/assets/scripts/GameMap.js
+++ web/src/assets/scripts/GameMap.js
@@ -3,10 +3,11 @@ import { Wall } from "./Wall";
 import { Snake } from "./Snake";
 
 export class GameMap extends AcGameObject {
-    constructor(ctx, parent) {
+    constructor(ctx, parent, store) {
         super();
         this.ctx = ctx;
         this.parent = parent;
+        this.store = store;
         this.L = 0; // 一个小方格的边长
         this.rows = 13;
         this.cols = 14;
@@ -20,64 +21,14 @@ export class GameMap extends AcGameObject {
         ];
     }
 
-    check_connectivity(g, sx, sy, tx, ty) {
-        if (sx === tx && sy === ty) {
-            return true;
-        }
-        g[sx][sy] = true;
-        let dx = [0, 0, -1, 1],
-            dy = [1, -1, 0, 0];
-        for (let i = 0; i < 4; i++) {
-            let x = sx + dx[i],
-                y = sy + dy[i];
-            if (!g[x][y] && this.check_connectivity(g, x, y, tx, ty)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     create_walls() {
-        const g = [];
-        for (let i = 0; i < this.rows; i++) {
-            g[i] = [];
-            for (let j = 0; j < this.cols; j++) {
-                g[i][j] = false;
-            }
-        }
-        for (let i = 0; i < this.rows; i++) {
-            g[i][0] = g[i][this.cols - 1] = true;
-        }
-        for (let i = 0; i < this.cols; i++) {
-            g[0][i] = g[this.rows - 1][i] = true;
-        }
-
-        for (let i = 0; i < this.inner_walls_cnt / 2; i++) {
-            for (let j = 0; j < this.rand_cnt; j++) {
-                let r = parseInt(Math.random() * this.rows);
-                let c = parseInt(Math.random() * this.cols);
-                if (g[r][c] || g[this.rows - 1 - r][this.cols - 1 - c])
-                    continue;
-                if (
-                    (r === this.rows - 2 && c === 1) ||
-                    (r === 1 && c === this.cols - 2)
-                )
-                    continue;
-                g[r][c] = g[this.rows - 1 - r][this.cols - 1 - c] = true;
-                break;
-            }
-        }
-
-        const g_bk = JSON.parse(JSON.stringify(g));
-        if (!this.check_connectivity(g_bk, this.rows - 2, 1, 1, this.cols - 2))
-            return false;
-
+        const g = this.store.state.pk.gamemap;
+        
         for (let i = 0; i < this.rows; i++) {
             for (let j = 0; j < this.cols; j++) {
                 if (g[i][j]) this.walls.push(new Wall(i, j, this));
             }
         }
-        return true;
     }
 
     update_size() {
@@ -113,9 +64,7 @@ export class GameMap extends AcGameObject {
     }
 
     start() {
-        for (let i = 0; i < this.rand_cnt; i++) {
-            if (this.create_walls()) break;
-        }
+        this.create_walls();
         this.add_listening_events();
     }
 
