518505dc593a88a1870a426d258d5a796d110a89
Yimin Zhang <18796895416@163.com>
2024-04-04 00:47:30 +0800

    实现matchingpool 实现backend的startGame接口

--- backendcloud/matchingsystem/src/main/java/com/kob/matchingsystem/MatchingSystemApplication.java
+++ backendcloud/matchingsystem/src/main/java/com/kob/matchingsystem/MatchingSystemApplication.java
@@ -2,10 +2,12 @@ package com.kob.matchingsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.kob.matchingsystem.service.impl.MathcingServiceImpl;
 
 @SpringBootApplication
 public class MatchingSystemApplication {
     public static void main(String[] args) {
+        MathcingServiceImpl.matchingPool.start();
         SpringApplication.run(MatchingSystemApplication.class, args);
     }
 }
