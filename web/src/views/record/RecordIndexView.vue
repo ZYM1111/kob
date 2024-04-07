c0e81d8dd9ea4182eeb27a854eacd6ca686f8fea
Yimin Zhang <18796895416@163.com>
2024-04-07 18:07:12 +0800

    getRecordList接口实现

--- web/src/views/record/RecordIndexView.vue
+++ web/src/views/record/RecordIndexView.vue
@@ -6,9 +6,38 @@
 
 <script>
 import ContentField from '../../components/ContentField.vue';
+import $ from 'jquery';
+import { useStore } from 'vuex';
+
 export default {
     components: {
         ContentField
+    },
+    setup() {
+        const store = useStore();
+        let current_page = 1;
+
+        const pull_page = page => {
+            current_page = page;
+            $.ajax({
+                url: "http://127.0.0.1:3000/record/getlist/",
+                type: "GET",
+                data: {
+                    page: page,
+                },
+                headers: {
+                    Authorization: "Bearer " + store.state.user.token,
+                },
+                success(resp) {
+                    console.log(resp);
+                },
+                error(resp) {
+                    console.log(resp);
+                }
+            });
+        }
+
+        pull_page(current_page);
     }
 }
 </script>
